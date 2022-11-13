006d7988b5b08470a82800b03d8148308239f34a
kasra <k.sorouri@gmail.com>
2022-11-13 16:34:16 +0200

    End of Exercise 3.3

--- index.js
+++ index.js
@@ -35,6 +35,18 @@ app.get('/info',(req, res) =>{
             <p>${time.toString()}</p>`)
 })
 
+app.get('/api/persons/:id',(req, res) =>{
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404)
+    .end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => 
-    console.log(`Server is r'unning on the port ${PORT}`))
+    console.log(`Server is running on the port ${PORT}`))
