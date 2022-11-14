27dc876f5e79ce66f1c84231eae0238c3c370fc0
kasra <k.sorouri@gmail.com>
2022-11-14 10:43:17 +0200

    End of Exercise 3.10

--- index.js
+++ index.js
@@ -84,6 +84,6 @@ app.post('/api/persons/',(req, res) => {
 }) 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => 
     console.log(`Server is running on the port ${PORT}`))
